0a290b81b6cee1adc2d507c005879ed188121426
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-01 15:48:05 -0400

    fixed wget link

--- gitlab-installation.sh
+++ gitlab-installation.sh
@@ -49,7 +49,7 @@ yum install cronie
 echo "Retrieving and installing GitLab rpm"
 RPM_FILE=gitlab-ce-7.10.4~omnibus.1.x86_64.rpm
 cd /home/$USER
-wget https://downloads-packages.s3.amazonaws.com/centos-7.1.1503/gitlab-ce-7.10.4~omnibus01.x86_64.rpm --directory-prefix=/home/$USER
+wget https://downloads-packages.s3.amazonaws.com/centos-7.1.1503/gitlab-ce-7.10.4~omnibus-1.x86_64.rpm --directory-prefix=/home/$USER
 if [ -f /home/$USER/$RPM_FILE ];
 then
   echo "Installing GitLab rpm"
